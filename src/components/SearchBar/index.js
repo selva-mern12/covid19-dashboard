83792ee38a946bd482e264f318ede7db257acdb5
selva-mern12 <selva.mern12@gmail.com>
2025-02-15 14:22:20 +0000

    Final commit

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -44,25 +44,27 @@ class SearchBar extends Component {
                   testid="searchResultsUnorderedList"
                 >
                   {filterStateList.map(state => (
-                    <li
-                      key={state.state_code}
-                      className="search-bar-list"
-                      onClick={() =>
-                        this.setState({userSearch: state.state_name})
-                      }
+                    <Link
+                      to={`/state/${state.state_code}`}
+                      className="nav-link"
                     >
-                      <p>{state.state_name}</p>
-                      <Link
-                        to={`/state/${state.state_code}`}
-                        className="state-code"
+                      <li
+                        key={state.state_code}
+                        className="search-bar-list"
+                        onClick={() =>
+                          this.setState({userSearch: state.state_name})
+                        }
                       >
-                        <p>{state.state_code}</p>
-                        <BiChevronRightSquare
-                          size={17}
-                          testid={`moveTo-${state.state_code}`}
-                        />
-                      </Link>
-                    </li>
+                        <p>{state.state_name}</p>
+                        <div className="state-code">
+                          <p>{state.state_code}</p>
+                          <BiChevronRightSquare
+                            size={17}
+                            testid={`moveTo-${state.state_code}`}
+                          />
+                        </div>
+                      </li>
+                    </Link>
                   ))}
                 </ul>
               )}
